250df4831c437a109b9b2d9e3a8a52f17a955908
Piotr Osipa <piotr.osipa@gmail.com>
2017-03-05 18:16:59 +0100

    przygotowanie tekstu w pliku do tokenizacji

--- src/staticc/Tokenization.java
+++ src/staticc/Tokenization.java
@@ -1,8 +1,12 @@
 package staticc;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javafx.util.Pair;
 import model.Project;
 import model.Projects;
 import model.token.TokenFile;
@@ -10,18 +14,106 @@ import model.token.TokenLine;
 
 public final class Tokenization {
 
-    public static TokenFile tokenization(File file) {
-        File prepareFile = prepareFileForTokenization(file);
+    private static boolean testPrint = true;
 
-        // TODO: tokenizacja przygotowanego pliku
+    public static TokenFile tokenization(File file) {
+        String text = cleanUpFile(file);
+
+        // TODO: usun
+        if (file.getPath().contains("Algorytmy L Cw1\\Plecak.cpp") && testPrint == true) {
+            testPrint = false;
+            System.out.println(text);
+        }
+
+        // TODO: tokenizacja
         ArrayList<TokenLine> tokenLines = new ArrayList<>();
         TokenFile tokenFile = new TokenFile(tokenLines);
         return tokenFile;
     }
 
-    private static File prepareFileForTokenization(File file) {
+    private static String cleanUpFile(File file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            StringBuilder sb = new StringBuilder();
 
-        return null;
+            int lineNumber = 1;
+            boolean commentStarted = false;
+            String line = br.readLine();
+            while (line != null) {
+                // Nie przestawiać kolejności wywoływania instrukcji
+                line = clearText(line);
+
+                Pair<String, Boolean> pair = clearComments(line, commentStarted);
+                line = pair.getKey();
+                commentStarted = pair.getValue();
+
+                line = line.trim();
+                if ((line.startsWith("#") || line.startsWith("using"))) { // #include, #endif, using std::cout
+                    line = "";
+                }
+
+                sb.append(lineNumber + "\t" + line + "\n");
+                lineNumber++; // TODO: usun
+                line = br.readLine();
+            }
+
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String clearText(String line) {
+        if (line.contains("\"")) {  // "tes\"st" = ""
+            line = line.replace("\\\"", "");
+            line = line.replaceAll("\".*?\"", "\"\"");
+        }
+        if (line.contains("\'")) {  // '\'' = ''
+            line = line.replace("\\\'", "");
+            line = line.replaceAll("\'.*?\'", "\'\'");
+        }
+
+        return line;
+    }
+
+    private static Pair<String, Boolean> clearComments(String line, boolean commentStarted) {
+        while (true) {
+            int commentStart = line.indexOf("/*");
+            int commentEnd = line.indexOf("*/");
+
+            if (line.contains("//")) {
+                line = line.replaceAll("\\/\\/.*", "");
+                continue;
+            }
+
+            if (commentStart != -1 && commentEnd == -1) {       // Komentarz sie zaczyna
+                line = line.replaceAll("\\/\\*.*", "");
+                commentStarted = true;
+                continue;
+            }
+            else if (commentStart == -1 && commentEnd != -1) {  // Komentarz sie konczy
+                line = line.replaceAll(".*\\*\\/", "");
+                commentStarted = false;
+                continue;
+            }
+            else if (commentStart < commentEnd) {               // Komentarz sie zaczyna i konczy
+                line = line.replaceAll("\\/\\*.*\\*\\/", "");
+                continue;
+            }
+            else if (commentStart > commentEnd) {               // komentarz konczy sie i zaczyna
+                line = line.replaceAll("\\*\\/.*\\/\\*", "");
+                continue;
+            }
+            else if (commentStarted == true) {
+                line = "";
+                break;
+            }
+            else {
+                break;
+            }
+        }
+
+        return new Pair<>(line, commentStarted);
     }
 
     public static ArrayList<TokenFile> tokenProject(Project project) {
