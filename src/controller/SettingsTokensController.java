c09a0ab1b9acfa90f6fbc4efe65ad29d5481caff
Piotr Osipa <piotr.osipa@gmail.com>
2017-04-29 17:25:56 +0200

    zapisanie i czytanie ustawien tokenow

--- src/controller/SettingsTokensController.java
+++ src/controller/SettingsTokensController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.stage.Stage;
+import staticc.Settings;
 
 public class SettingsTokensController implements Initializable {
 
@@ -22,25 +23,81 @@ public class SettingsTokensController implements Initializable {
     @FXML private CheckBox iClass, iNew, iEnum, iExtends, iImplements;
     @FXML private CheckBox iTry, iCatch, iThrow;
     @FXML private CheckBox iBreak, iContinue, iReturn;
-    @FXML private CheckBox iSkipFuntionArgs, iSkipStatmentArgs, iSkipLoopArgs;
+    @FXML private CheckBox iSkipFunctionArgs, iSkipStatmentArgs, iSkipLoopArgs;
 
     private Stage stage;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        iNumber.setSelected(Settings.getINunmber());
+        iNumberWhole.setSelected(Settings.getINumberWhole());
+        iNumberDecimal.setSelected(Settings.getINumberDecimal());
+        iInt.setSelected(Settings.getIInt());
+        iLong.setSelected(Settings.getILong());
+        iShort.setSelected(Settings.getIShort());
+        iFloat.setSelected(Settings.getIFloat());
+        iDouble.setSelected(Settings.getIDouble());
+        iClassVariable.setSelected(Settings.getIClassVariable());
+        iBoolean.setSelected(Settings.getIBoolean());
+        iByte.setSelected(Settings.getIByte());
+        iOperation.setSelected(Settings.getIOperation());
+        iRelation.setSelected(Settings.getIRelation());
+        iAssign.setSelected(Settings.getIAssign());
+        iLogic.setSelected(Settings.getILogic());
+        iArithmetic.setSelected(Settings.getIArithmetic());
+        iBitwise.setSelected(Settings.getIBitwise());
+        iIfAndElse.setSelected(Settings.getIIfAndElse());
+        iSwitch.setSelected(Settings.getISwitch());
+        iCase.setSelected(Settings.getICase());
+        iDefault.setSelected(Settings.getIDefault());
+        iLoop.setSelected(Settings.getILoop());
+        iFor.setSelected(Settings.getIFor());
+        iWhile.setSelected(Settings.getIWhile());
+        iDo.setSelected(Settings.getIDo());
+        iFunctionDefine.setSelected(Settings.getIFunctionDefine());
+        iFunctionUse.setSelected(Settings.getIFunctionUse());
+        iConstructorUse.setSelected(Settings.getIConstructorUse());
+        iStatic.setSelected(Settings.getIStatic());
+        iFinal.setSelected(Settings.getIFinal());
+        iThrows.setSelected(Settings.getIThrows());
+        iVoid.setSelected(Settings.getIVoid());
+        iTable.setSelected(Settings.getITable());
+        iCast.setSelected(Settings.getICast());
+        iGeneric.setSelected(Settings.getIGeneric());
+        iClass.setSelected(Settings.getIClass());
+        iNew.setSelected(Settings.getINew());
+        iEnum.setSelected(Settings.getIEnum());
+        iExtends.setSelected(Settings.getIExtends());
+        iImplements.setSelected(Settings.getIImplements());
+        iTry.setSelected(Settings.getITry());
+        iCatch.setSelected(Settings.getICatch());
+        iThrow.setSelected(Settings.getIThrow());
+        iBreak.setSelected(Settings.getIBreak());
+        iContinue.setSelected(Settings.getIContinue());
+        iReturn.setSelected(Settings.getIReturn());
+        iSkipFunctionArgs.setSelected(Settings.getISkipFunctionArgs());
+        iSkipStatmentArgs.setSelected(Settings.getISkipStatmentArgs());
+        iSkipLoopArgs.setSelected(Settings.getISkipLoopArgs());
 
+        updateNumber();
+        updateOperation();
+        updateLoop();
     }
 
-    @FXML
+    @FXML @SuppressWarnings("unused")
     private void actionNumber(ActionEvent event) {
+        updateNumber();
+    }
+
+    private void updateNumber() {
         if (iNumber.isSelected()) {
             iNumberWhole.setDisable(true);
             iNumberDecimal.setDisable(true);
 
             iNumberWhole.setSelected(true);
             iNumberDecimal.setSelected(true);
-            actionNumberWhole(event);
-            actionNumberDecimal(event);
+            updateNumberWhole();
+            updateNumberDecimal();
             iNumberWhole.setSelected(false);
             iNumberDecimal.setSelected(false);
         }
@@ -51,13 +108,17 @@ public class SettingsTokensController implements Initializable {
             iNumberWhole.setSelected(true);
             iNumberDecimal.setSelected(true);
 
-            actionNumberWhole(event);
-            actionNumberDecimal(event);
+            updateNumberWhole();
+            updateNumberDecimal();
         }
     }
 
     @FXML @SuppressWarnings("unused")
     private void actionNumberWhole(ActionEvent event) {
+        updateNumberWhole();
+    }
+
+    private void updateNumberWhole() {
         if (iNumberWhole.isSelected()) {
             iInt.setDisable(true);
             iLong.setDisable(true);
@@ -80,6 +141,10 @@ public class SettingsTokensController implements Initializable {
 
     @FXML @SuppressWarnings("unused")
     private void actionNumberDecimal(ActionEvent event) {
+        updateNumberDecimal();
+    }
+
+    private void updateNumberDecimal() {
         if (iNumberDecimal.isSelected()) {
             iFloat.setDisable(true);
             iDouble.setDisable(true);
@@ -98,6 +163,10 @@ public class SettingsTokensController implements Initializable {
 
     @FXML @SuppressWarnings("unused")
     private void actionOperation(ActionEvent event) {
+        updateOperation();
+    }
+
+    private void updateOperation() {
         if (iOperation.isSelected()) {
             iRelation.setDisable(true);
             iAssign.setDisable(true);
@@ -128,6 +197,10 @@ public class SettingsTokensController implements Initializable {
 
     @FXML @SuppressWarnings("unused")
     private void actionLoop(ActionEvent event) {
+        updateLoop();
+    }
+
+    private void updateLoop() {
         if (iLoop.isSelected()) {
             iFor.setDisable(true);
             iWhile.setDisable(true);
@@ -151,6 +224,56 @@ public class SettingsTokensController implements Initializable {
     @FXML @SuppressWarnings("unused")
     private void actionOk(ActionEvent event) {
         stage.close();
+
+        Settings.setINumber(iNumber.isSelected());
+        Settings.setINumberWhole(iNumberWhole.isSelected());
+        Settings.setINumberDecimal(iNumberDecimal.isSelected());
+        Settings.setIInt(iInt.isSelected());
+        Settings.setILong(iLong.isSelected());
+        Settings.setIShort(iShort.isSelected());
+        Settings.setIFloat(iFloat.isSelected());
+        Settings.setIDouble(iDouble.isSelected());
+        Settings.setIClassVariable(iClassVariable.isSelected());
+        Settings.setIBoolean(iBoolean.isSelected());
+        Settings.setIByte(iByte.isSelected());
+        Settings.setIOperation(iOperation.isSelected());
+        Settings.setIRelation(iRelation.isSelected());
+        Settings.setIAssign(iAssign.isSelected());
+        Settings.setILogic(iLogic.isSelected());
+        Settings.setIArithmetic(iArithmetic.isSelected());
+        Settings.setIBitwise(iBitwise.isSelected());
+        Settings.setIIfAndElse(iIfAndElse.isSelected());
+        Settings.setISwitch(iSwitch.isSelected());
+        Settings.setICase(iCase.isSelected());
+        Settings.setIDefault(iDefault.isSelected());
+        Settings.setILoop(iLoop.isSelected());
+        Settings.setIFor(iFor.isSelected());
+        Settings.setIWhile(iWhile.isSelected());
+        Settings.setIDo(iDo.isSelected());
+        Settings.setIFunctionDefine(iFunctionDefine.isSelected());
+        Settings.setIFunctionUse(iFunctionUse.isSelected());
+        Settings.setIConstructorUse(iConstructorUse.isSelected());
+        Settings.setIStatic(iStatic.isSelected());
+        Settings.setIFinal(iFinal.isSelected());
+        Settings.setIThrows(iThrows.isSelected());
+        Settings.setIVoid(iVoid.isSelected());
+        Settings.setITable(iTable.isSelected());
+        Settings.setICast(iCast.isSelected());
+        Settings.setIGeneric(iGeneric.isSelected());
+        Settings.setIClass(iClass.isSelected());
+        Settings.setINew(iNew.isSelected());
+        Settings.setIEnum(iEnum.isSelected());
+        Settings.setIExtends(iExtends.isSelected());
+        Settings.setIImplements(iImplements.isSelected());
+        Settings.setITry(iTry.isSelected());
+        Settings.setICatch(iCatch.isSelected());
+        Settings.setIThrow(iThrow.isSelected());
+        Settings.setIBreak(iBreak.isSelected());
+        Settings.setIContinue(iContinue.isSelected());
+        Settings.setIReturn(iReturn.isSelected());
+        Settings.setISkipFunctionArgs(iSkipFunctionArgs.isSelected());
+        Settings.setISkipStatmentArgs(iSkipStatmentArgs.isSelected());
+        Settings.setISkipLoopArgs(iSkipLoopArgs.isSelected());
     }
 
     @FXML @SuppressWarnings("unused")
--- src/staticc/Settings.java
+++ src/staticc/Settings.java
@@ -15,6 +15,19 @@ public final class Settings {
     private static final String MINIMAL_MATCHED_LINES_VALUE = "minimal_matched_lines_value";
     private static final int MINIMAL_MATCHED_LINES_VALUE_DEFAULT = 5;
 
+    private static String I_NUMBER = "i_number", I_NUMBER_WHOLE = "i_number_whole", I_NUMBER_DECIMAL = "i_number_decimal", I_INT = "i_int", I_LONG = "i_long", I_SHORT = "i_short", I_FLOAT = "i_float", I_DOUBLE = "i_double";
+    private static String I_CLASS_VARIABLE = "i_class_variable", I_BOOLEAN = "i_boolean", I_BYTE = "i_byte";
+    private static String I_OPERATION = "i_operation", I_RELATION = "i_relation", I_ASSIGN = "i_assign", I_LOGIC = "i_logic", I_ARITHMETIC = "i_arithmetic", I_BITWISE = "i_bitwise";
+    private static String I_IF_AND_ELSE = "i_if_and_else", I_SWITCH = "i_swtich", I_CASE = "i_case", I_DEFAULT = "i_default";
+    private static String I_LOOP = "i_loop", I_FOR = "i_for", I_WHILE = "i_while", I_DO = "i_do";
+    private static String I_FUNCTION_DEFINE = "i_function_define", I_FUNCTION_USE = "i_function_use", I_CONSTRUCTOR_USE = "i_constructor_use";
+    private static String I_STATIC = "i_static", I_FINAL = "i_final", I_THROWS = "i_throws", I_VOID = "i_void";
+    private static String I_TABLE = "i_table", I_CAST = "i_cast", I_GENERIC = "i_generic";
+    private static String I_CLASS = "i_class", I_NEW = "i_new", I_ENUM = "i_enum", I_EXTENDS = "i_extends", I_IMPLEMETNS = "i_implements";
+    private static String I_TRY = "i_try", I_CATCH = "i_catch", I_THROW = "i_throw";
+    private static String I_BREAK = "i_break", I_CONTINUE = "i_continue", I_RETURN = "i_return";
+    private static String I_SKIP_FUNCTION_ARGS = "i_skip_function_args", I_SKIP_STATMENT_ARGS = "i_skip_statment_args", I_SKIP_LOOP_ARGS = "i_skip_loop_args";
+
     private Settings() {
 
     }
@@ -57,8 +70,6 @@ public final class Settings {
         return basePath;
     }
 
-
-
     public static Projects getBase() {
         return loadBase();
     }
@@ -81,14 +92,404 @@ public final class Settings {
         }
     }
 
-
-
     public static int getMinimalMatchedLinesValue() {
         return preferences.getInt(MINIMAL_MATCHED_LINES_VALUE, MINIMAL_MATCHED_LINES_VALUE_DEFAULT);
     }
 
     public static void setMinimalMatchedLinesValue(int value) {
         preferences.putInt(MINIMAL_MATCHED_LINES_VALUE, value);
+    }
+
+    public static boolean getINunmber() {
+        return preferences.getBoolean(I_NUMBER, false);
+    }
+
+    public static void setINumber(boolean value) {
+        preferences.putBoolean(I_NUMBER, value);
+    }
+
+    public static boolean getINumberWhole() {
+        return preferences.getBoolean(I_NUMBER_WHOLE, true);
+    }
+
+    public static void setINumberWhole(boolean value) {
+        preferences.putBoolean(I_NUMBER_WHOLE, value);
+    }
+
+    public static boolean getINumberDecimal() {
+        return preferences.getBoolean(I_NUMBER_DECIMAL, true);
+    }
+
+    public static void setINumberDecimal(boolean value) {
+        preferences.putBoolean(I_NUMBER_DECIMAL, value);
+    }
+
+    public static boolean getIInt() {
+        return preferences.getBoolean(I_INT, false);
+    }
+
+    public static void setIInt(boolean value) {
+        preferences.putBoolean(I_INT, value);
+    }
+
+    public static boolean getILong() {
+        return preferences.getBoolean(I_LONG, false);
+    }
+
+    public static void setILong(boolean value) {
+        preferences.putBoolean(I_LONG, value);
+    }
+
+    public static boolean getIShort() {
+        return preferences.getBoolean(I_SHORT, false);
+    }
+
+    public static void setIShort(boolean value) {
+        preferences.putBoolean(I_SHORT, value);
+    }
+
+    public static boolean getIFloat() {
+        return preferences.getBoolean(I_FLOAT, false);
+    }
+
+    public static void setIFloat(boolean value) {
+        preferences.putBoolean(I_FLOAT, value);
+    }
+
+    public static boolean getIDouble() {
+        return preferences.getBoolean(I_DOUBLE, false);
+    }
+
+    public static void setIDouble(boolean value) {
+        preferences.putBoolean(I_DOUBLE, value);
+    }
+
+    public static boolean getIClassVariable() {
+        return preferences.getBoolean(I_CLASS_VARIABLE, true);
+    }
+
+    public static void setIClassVariable(boolean value) {
+        preferences.putBoolean(I_CLASS_VARIABLE, value);
+    }
+
+    public static boolean getIBoolean() {
+        return preferences.getBoolean(I_BOOLEAN, true);
+    }
+
+    public static void setIBoolean(boolean value) {
+        preferences.putBoolean(I_BOOLEAN, value);
+    }
+
+    public static boolean getIByte() {
+        return preferences.getBoolean(I_BYTE, true);
+    }
+
+    public static void setIByte(boolean value) {
+        preferences.putBoolean(I_BYTE, value);
+    }
+
+    public static boolean getIOperation() {
+        return preferences.getBoolean(I_OPERATION, false);
+    }
+
+    public static void setIOperation(boolean value) {
+        preferences.putBoolean(I_OPERATION, value);
+    }
+
+    public static boolean getIRelation() {
+        return preferences.getBoolean(I_RELATION, true);
+    }
+
+    public static void setIRelation(boolean value) {
+        preferences.putBoolean(I_RELATION, value);
+    }
+
+    public static boolean getIAssign() {
+        return preferences.getBoolean(I_ASSIGN, true);
+    }
+
+    public static void setIAssign(boolean value) {
+        preferences.putBoolean(I_ASSIGN, value);
+    }
+
+    public static boolean getILogic() {
+        return preferences.getBoolean(I_LOGIC, true);
+    }
+
+    public static void setILogic(boolean value) {
+        preferences.putBoolean(I_LOGIC, value);
+    }
+
+    public static boolean getIArithmetic() {
+        return preferences.getBoolean(I_ARITHMETIC, true);
+    }
+
+    public static void setIArithmetic(boolean value) {
+        preferences.putBoolean(I_ARITHMETIC, value);
+    }
+
+    public static boolean getIBitwise() {
+        return preferences.getBoolean(I_BITWISE, true);
+    }
+
+    public static void setIBitwise(boolean value) {
+        preferences.putBoolean(I_BITWISE, value);
+    }
+
+    public static boolean getIIfAndElse() {
+        return preferences.getBoolean(I_IF_AND_ELSE, true);
+    }
+
+    public static void setIIfAndElse(boolean value) {
+        preferences.putBoolean(I_IF_AND_ELSE, value);
+    }
+
+    public static boolean getISwitch() {
+        return preferences.getBoolean(I_SWITCH, true);
+    }
+
+    public static void setISwitch(boolean value) {
+        preferences.putBoolean(I_SWITCH, value);
+    }
+
+    public static boolean getICase() {
+        return preferences.getBoolean(I_CASE, true);
+    }
+
+    public static void setICase(boolean value) {
+        preferences.putBoolean(I_CASE, value);
+    }
+
+    public static boolean getIDefault() {
+        return preferences.getBoolean(I_DEFAULT, true);
+    }
+
+    public static void setIDefault(boolean value) {
+        preferences.putBoolean(I_DEFAULT, value);
+    }
+
+    public static boolean getILoop() {
+        return preferences.getBoolean(I_LOOP, false);
+    }
+
+    public static void setILoop(boolean value) {
+        preferences.putBoolean(I_LOOP, value);
+    }
+
+    public static boolean getIFor() {
+        return preferences.getBoolean(I_FOR, true);
+    }
+
+    public static void setIFor(boolean value) {
+        preferences.putBoolean(I_FOR, value);
+    }
+
+    public static boolean getIWhile() {
+        return preferences.getBoolean(I_WHILE, true);
+    }
+
+    public static void setIWhile(boolean value) {
+        preferences.putBoolean(I_WHILE, value);
+    }
+
+    public static boolean getIDo() {
+        return preferences.getBoolean(I_DO, true);
+    }
+
+    public static void setIDo(boolean value) {
+        preferences.putBoolean(I_DO, value);
+    }
+
+    public static boolean getIFunctionDefine() {
+        return preferences.getBoolean(I_FUNCTION_DEFINE, true);
+    }
+
+    public static void setIFunctionDefine(boolean value) {
+        preferences.putBoolean(I_FUNCTION_DEFINE, value);
+    }
+
+    public static boolean getIFunctionUse() {
+        return preferences.getBoolean(I_FUNCTION_USE, true);
+    }
+
+    public static void setIFunctionUse(boolean value) {
+        preferences.putBoolean(I_FUNCTION_USE, value);
+    }
+
+    public static boolean getIConstructorUse() {
+        return preferences.getBoolean(I_CONSTRUCTOR_USE, true);
+    }
+
+    public static void setIConstructorUse(boolean value) {
+        preferences.putBoolean(I_CONSTRUCTOR_USE, value);
+    }
+
+    public static boolean getIStatic() {
+        return preferences.getBoolean(I_STATIC, true);
+    }
+
+    public static void setIStatic(boolean value) {
+        preferences.putBoolean(I_STATIC, value);
+    }
+
+    public static boolean getIFinal() {
+        return preferences.getBoolean(I_FINAL, true);
+    }
+
+    public static void setIFinal(boolean value) {
+        preferences.putBoolean(I_FINAL, value);
+    }
+
+    public static boolean getIThrows() {
+        return preferences.getBoolean(I_THROWS, true);
+    }
+
+    public static void setIThrows(boolean value) {
+        preferences.putBoolean(I_THROWS, value);
+    }
+
+    public static boolean getIVoid() {
+        return preferences.getBoolean(I_VOID, true);
+    }
+
+    public static void setIVoid(boolean value) {
+        preferences.putBoolean(I_VOID, value);
+    }
+
+    public static boolean getITable() {
+        return preferences.getBoolean(I_TABLE, true);
+    }
+
+    public static void setITable(boolean value) {
+        preferences.putBoolean(I_TABLE, value);
+    }
+
+    public static boolean getICast() {
+        return preferences.getBoolean(I_CAST, true);
+    }
+
+    public static void setICast(boolean value) {
+        preferences.putBoolean(I_CAST, value);
+    }
+
+    public static boolean getIGeneric() {
+        return preferences.getBoolean(I_GENERIC, true);
+    }
+
+    public static void setIGeneric(boolean value) {
+        preferences.putBoolean(I_GENERIC, value);
+    }
+
+    public static boolean getIClass() {
+        return preferences.getBoolean(I_CLASS, true);
+    }
+
+    public static void setIClass(boolean value) {
+        preferences.putBoolean(I_CLASS, value);
+    }
+
+    public static boolean getINew() {
+        return preferences.getBoolean(I_NEW, true);
+    }
+
+    public static void setINew(boolean value) {
+        preferences.putBoolean(I_NEW, value);
+    }
+
+    public static boolean getIEnum() {
+        return preferences.getBoolean(I_ENUM, true);
+    }
+
+    public static void setIEnum(boolean value) {
+        preferences.putBoolean(I_ENUM, value);
+    }
+
+    public static boolean getIExtends() {
+        return preferences.getBoolean(I_EXTENDS, true);
+    }
+
+    public static void setIExtends(boolean value) {
+        preferences.putBoolean(I_EXTENDS, value);
+    }
+
+    public static boolean getIImplements() {
+        return preferences.getBoolean(I_IMPLEMETNS, true);
+    }
+
+    public static void setIImplements(boolean value) {
+        preferences.putBoolean(I_IMPLEMETNS, value);
+    }
+
+    public static boolean getITry() {
+        return preferences.getBoolean(I_TRY, true);
+    }
+
+    public static void setITry(boolean value) {
+        preferences.putBoolean(I_TRY, value);
+    }
+
+    public static boolean getICatch() {
+        return preferences.getBoolean(I_CATCH, true);
+    }
+
+    public static void setICatch(boolean value) {
+        preferences.putBoolean(I_CATCH, value);
+    }
+
+    public static boolean getIThrow() {
+        return preferences.getBoolean(I_THROW, true);
+    }
+
+    public static void setIThrow(boolean value) {
+        preferences.putBoolean(I_THROW, value);
+    }
+
+    public static boolean getIBreak() {
+        return preferences.getBoolean(I_BREAK, true);
+    }
+
+    public static void setIBreak(boolean value) {
+        preferences.putBoolean(I_BREAK, value);
+    }
+
+    public static boolean getIContinue() {
+        return preferences.getBoolean(I_CONTINUE, true);
+    }
+
+    public static void setIContinue(boolean value) {
+        preferences.putBoolean(I_CONTINUE, value);
+    }
+
+    public static boolean getIReturn() {
+        return preferences.getBoolean(I_RETURN, true);
+    }
+
+    public static void setIReturn(boolean value) {
+        preferences.putBoolean(I_RETURN, value);
+    }
+
+    public static boolean getISkipFunctionArgs() {
+        return preferences.getBoolean(I_SKIP_FUNCTION_ARGS, false);
+    }
+
+    public static void setISkipFunctionArgs(boolean value) {
+        preferences.putBoolean(I_SKIP_FUNCTION_ARGS, value);
+    }
+
+    public static boolean getISkipStatmentArgs() {
+        return preferences.getBoolean(I_SKIP_STATMENT_ARGS, true);
+    }
+
+    public static void setISkipStatmentArgs(boolean value) {
+        preferences.putBoolean(I_SKIP_STATMENT_ARGS, value);
+    }
+
+    public static boolean getISkipLoopArgs() {
+        return preferences.getBoolean(I_SKIP_LOOP_ARGS, true);
+    }
+
+    public static void setISkipLoopArgs(boolean value) {
+        preferences.putBoolean(I_SKIP_LOOP_ARGS, value);
     }
 
 }
